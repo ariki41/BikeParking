63abf0cc47f284a564be401e7d710236fb010f0c
ariki41 <lampyridae041@gmail.com>
2025-10-02 19:39:17 +0900

    ADD: 駐車場編集機能を追加、sessionで管理するように変更

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -14,5 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (MethodNotAllowedHttpException $e) {
+            return redirect()->route('home');
+        });
     })->create();
